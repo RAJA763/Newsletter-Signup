e8947d273d99a33ee004a5f863e637499f76f3e0
Raja <raja3irty@gmail.com>
2023-06-29 17:32:51 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
+//jshint esversion: 6
+
 const express = require("express");
-const bodyPraser = require("body-parser");
+const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 
-const app = express();
+const app= express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyPraser.urlencoded({extended :true}));
-
-app.get("/",function(req, res){
+app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req,res){
+app.post("/", function(req, res){
     const firstName = req.body.firstName;
-    const email = req.body.email;
     const lastName = req.body.lastName;
-    
+    const email = req.body.email;
+
     const data = {
-        member : [
+        members: [
             {
                 email_address: email,
                 status: "subscribed",
@@ -30,18 +31,18 @@ app.post("/", function(req,res){
             }
         ]
     };
+
     const jsonData = JSON.stringify(data);
 
-    const url = "https://us11.api.mailchimp.com/3.0/lists/b07436f6d1";
+    const url = "https://us10.api.mailchimp.com/3.0/lists/b3ef8cadf0";
 
-    
-    const option = {
+    const options = {
         method: "POST",
-        auth: "Raja1:d15f9f3d7ce11c40213b3c957d2956a5-us11"
+        auth: "Raja1:3e799803b501631254c2a02af8d7c514-us10"
     }
 
-    const request = https.request(url, option, function(response){
-
+    const request = https.request(url, options, function(response) {
+        
         if(response.statusCode === 200){
             res.sendFile(__dirname + "/success.html");
         }
@@ -49,26 +50,38 @@ app.post("/", function(req,res){
             res.sendFile(__dirname + "/failure.html");
         }
 
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
-        });
+        })
     });
 
     request.write(jsonData);
+
     request.end();
 });
 
-app.post("/failure", function(req,res){
+app.post("/failure", function(req, res){
     res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, function(){
-    console.log("Server is running on port 3000");
+    console.log("Server is started in port 3000.");
 });
 
-//API key
-//d15f9f3d7ce11c40213b3c957d2956a5-us11
+// //old
+// //API key
+// //d15f9f3d7ce11c40213b3c957d2956a5-us11
+// //a6d5dedc01e9cde4d3d9cc96eee93706-us11
+//195a09440b3cca7804e8927100cf3ef7-us11
 
-//list id
-//b07436f6d1
+// //list id
+// //b07436f6d1
+
+// //new
+// //API key
+// //3e799803b501631254c2a02af8d7c514-us10
+
+// //listid
+// //b3ef8cadf0
 
